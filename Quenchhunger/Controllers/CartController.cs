ad1c1e96b39b49164e48942acb01983be7705c7c
nwbabu <32478442+nwbabu@users.noreply.github.com>
2019-08-11 12:25:49 +0530

    Remove item from cart

--- Quenchhunger/Controllers/CartController.cs
+++ Quenchhunger/Controllers/CartController.cs
@@ -9,10 +9,10 @@ namespace Quenchhunger.Controllers
     public class CartController : Controller
     {
         // GET: Cart
+        cartCheckOut checkout = new cartCheckOut();
+        List<CartDetails> cartlist = null;
         public ActionResult cart()
         {
-            cartCheckOut checkout = new cartCheckOut();
-            List<CartDetails> cartlist = null;
             if (Session["cartlist"] != null)
             {
                 cartlist = (List<CartDetails>)Session["cartlist"];
@@ -20,6 +20,20 @@ namespace Quenchhunger.Controllers
                 checkout.cartTotal = cartlist.Sum(x => x.price);
             }
             return View(checkout);
+        }
+        public ActionResult RemoveItemFromCart()
+        {
+            int itemId = Convert.ToInt32(Request.QueryString["itemId"].ToString());
+            if (Session["cartlist"] != null)
+            {
+                cartlist = (List<CartDetails>)Session["cartlist"];
+                CartDetails cartItem = cartlist.Find(x => x.productId == itemId);
+                cartlist.Remove(cartItem);
+                checkout.cartList = cartlist;
+                checkout.cartTotal = cartlist.Sum(x => x.price);
+                Session["cart"] = Convert.ToInt32(Session["cart"]) - 1;
+            }
+            return View("cart", checkout);
         }
     }
 }
