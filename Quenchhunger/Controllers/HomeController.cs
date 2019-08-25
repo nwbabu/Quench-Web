efb9f0b4155839ebdbd5331a591e1d6d68d6ddb0
nwbabu <32478442+nwbabu@users.noreply.github.com>
2019-08-25 20:48:49 +0530

    add home

--- Quenchhunger/Controllers/HomeController.cs
+++ Quenchhunger/Controllers/HomeController.cs
@@ -20,14 +20,10 @@ namespace Quenchhunger.Controllers
         }
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+           return View();
         }
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
         public ActionResult Restaurants()
